df1d06445745c8f6cb7a9a05d938410420fcee47
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-09-28 17:18:00 +0200

    RLE working

--- src/converter.cpp
+++ src/converter.cpp
@@ -5,7 +5,29 @@
 
 #include "converter.hpp"
 
-std::string runLengthEncode(std::string_view _number) 
+std::string runLengthEncode(const std::string_view _number) 
 {
-	return {};
+	size_t size = _number.size();
+	if(size == 0) return {};
+	std::string encoded;
+	unsigned groupCount = 1;
+	for(size_t i = 1; i < size; ++i)
+	{
+		if(_number[i-1] == _number[i])
+		{
+			++groupCount;	
+		}
+		else
+		{
+			encoded.append(std::to_string(groupCount))
+				.append(" ")
+				.append(1, _number[i-1])
+				.append(" ");
+			groupCount = 1;
+		}
+	}
+	encoded.append(std::to_string(groupCount))
+		.append(" ")
+		.append(1, _number[size-1]);
+	return encoded;
 }
