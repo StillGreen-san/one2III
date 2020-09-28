a38b22186125c6c0ff4475f7c13cc494cb15e3eb
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-09-28 16:18:47 +0200

    RLE base

--- src/converter.cpp
+++ src/converter.cpp
@@ -2,3 +2,10 @@
  * @file convert.cpp
  * @brief implementation file for conversion functions
  */
+
+#include "converter.hpp"
+
+std::string runLengthEncode(std::string_view _number) 
+{
+	return {};
+}
--- src/converter.hpp
+++ src/converter.hpp
@@ -2,3 +2,15 @@
  * @file convert.hpp
  * @brief include file for conversion functions
  */
+
+#pragma once
+
+#include <string>
+
+/**
+ * @brief returns a number sequence processed with RLE, space separated
+ * 
+ * @param _number
+ * @return std::string
+ */
+std::string runLengthEncode(std::string_view _number);
