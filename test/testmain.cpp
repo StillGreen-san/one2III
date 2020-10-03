ba4b39f6a7ab778dc603c2523e0f7210bb1e018b
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-10-03 12:41:55 +0200

    partition better check

--- test/testmain.cpp
+++ test/testmain.cpp
@@ -68,12 +68,16 @@ TEST_CASE("numberToEnglish")
 	CHECK(numberToEnglish("9875") == "");
 }
 
-TEST_CASE("integerPartitions") //TODO make unordered equal possible
+TEST_CASE("integerPartitions")
 {
 	using ptn = std::vector<std::vector<uint8_t>>;
 
-	CHECK(integerPartitions(0, 1) == ptn{});
-	CHECK(integerPartitions(5, 3) == ptn{{3,1,1},{2,2,1}});
-	CHECK(integerPartitions(7, 2) == ptn{{6,1},{5,2},{4,3}});
-	CHECK(integerPartitions(10, 4) == ptn{{7,1,1,1},{6,2,1,1},{5,3,1,1},{4,4,1,1},{5,2,2,1},{4,3,2,1},{3,3,3,1},{4,2,2,2},{3,3,2,2}});
+	CHECK_THAT(integerPartitions(0, 1), Catch::UnorderedEquals(ptn{}));
+	CHECK_THAT(integerPartitions(5, 3), Catch::UnorderedEquals(ptn{{3,1,1},{2,2,1}}));
+	CHECK_THAT(integerPartitions(7, 2), Catch::UnorderedEquals(ptn{{6,1},{5,2},{4,3}}));
+	CHECK_THAT(integerPartitions(16, 6, 2), Catch::UnorderedEquals(
+		ptn{{6,2,2,2,2,2},{5,3,2,2,2,2},{4,3,3,2,2,2},{3,3,3,3,2,2},{4,4,2,2,2,2}}));
+	CHECK_THAT(integerPartitions(10, 4), Catch::UnorderedEquals(
+		ptn{{7,1,1,1},{6,2,1,1},{5,3,1,1},{4,4,1,1},{5,2,2,1},{4,3,2,1},{3,3,3,1},{4,2,2,2},{3,3,2,2}}));
+	CHECK_THAT(integerPartitions(10, 4, 2, 3), Catch::UnorderedEquals(ptn{{3,3,2,2}}));
 }
