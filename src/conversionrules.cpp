d0db631281beda045760a72775cc4e70a90ff4df
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-10-09 12:49:59 +0200

    more rules, no test yet

--- src/conversionrules.cpp
+++ src/conversionrules.cpp
@@ -15,11 +15,14 @@ ConversionRule::ConversionRule(size_t _minInputSize, size_t _maxInputSize) :
 	
 }
 
-bool ConversionRule::canBeConverted(std::string_view _string) const
+size_t ConversionRule::getMinInputSize() const
 {
-	size_t size = _string.size();
-	return size >= minInputSize
-		&& size <= maxInputSize;
+	return minInputSize;
+}
+
+size_t ConversionRule::getMaxInputSize() const
+{
+	return maxInputSize;
 }
 
 #pragma endregion ConversionRule
@@ -35,15 +38,6 @@ RomanNumeralConversion::RomanNumeralConversion() :
 std::string RomanNumeralConversion::convert(std::string_view _string) const
 {
 	return romanNumerals(_string);
-}
-
-bool RomanNumeralConversion::canBeConverted(std::string_view _string) const 
-{
-	size_t size = _string.size();
-	if(size == maxInputSize)
-		return _string.front() <= '3';
-	return size >= minInputSize
-		&& size <= maxInputSize;
 }
 
 #pragma endregion RomanNumeralConversion
@@ -62,3 +56,63 @@ std::string RunLengthEncodingConversion::convert(std::string_view _string) const
 }
 
 #pragma endregion RunLengthEncodingConversion
+
+#pragma region NumberToEnglishConversion
+
+NumberToEnglishConversion::NumberToEnglishConversion() :
+	ConversionRule::ConversionRule(1, 3)
+{
+
+}
+
+std::string NumberToEnglishConversion::convert(std::string_view _string) const 
+{
+	return numberToEnglish(_string);
+}
+
+#pragma endregion NumberToEnglishConversion
+
+#pragma region AsRunLengthEncodingConversion
+
+AsRunLengthEncodingConversion::AsRunLengthEncodingConversion() :
+	ConversionRule::ConversionRule(2, std::numeric_limits<size_t>::max())
+{
+
+}
+
+std::string AsRunLengthEncodingConversion::convert(std::string_view _string) const
+{
+	return asRunLengthEncode(_string);
+}
+
+#pragma endregion AsRunLengthEncodingConversion
+
+#pragma region LookAndSay
+
+LookAndSay::LookAndSay() :
+	ConversionRule::ConversionRule(1, std::numeric_limits<size_t>::max())
+{
+
+}
+
+std::string LookAndSay::convert(std::string_view _string) const
+{
+	std::string runLengthEncoded = runLengthEncode(_string);
+	if(runLengthEncoded == "") return runLengthEncoded;
+	const size_t sectionSize = 2;
+	std::string separator = "";
+	std::string lookAndSay;
+	auto stringEnd = std::remove(begin(runLengthEncoded), end(runLengthEncoded), ' ');
+	auto sectionBegin = begin(runLengthEncoded);
+	while(sectionBegin != stringEnd)
+	{
+		lookAndSay
+			.append(separator)
+			.append(numberToEnglish(std::string_view(&*sectionBegin , sectionSize)));
+		separator = " ";
+		std::advance(sectionBegin, sectionSize);
+	}
+	return lookAndSay;
+}
+
+#pragma endregion LookAndSay
--- src/conversionrules.hpp
+++ src/conversionrules.hpp
@@ -18,13 +18,18 @@ public:
 	virtual std::string convert(std::string_view _string) const = 0;
 
 	/**
-	 * @brief checks if the string can be converted by this rule
+	 * @brief gets the minimum supported input size for conversion
 	 * 
-	 * @param _string the string to be converted
-	 * @return true if the string can be converted
-	 * @return false otherwise
+	 * @return size_t minimum supported input size for conversion
 	 */
-	virtual bool canBeConverted(std::string_view _string) const;
+	size_t getMinInputSize() const;
+
+	/**
+	 * @brief gets the maximum supported input size for conversion
+	 * 
+	 * @return size_t maximum supported input size for conversion
+	 */
+	size_t getMaxInputSize() const;
 
 protected:
 	ConversionRule(size_t _minInputSize, size_t _maxInputSize);
@@ -39,14 +44,36 @@ public:
 	RomanNumeralConversion::RomanNumeralConversion();
 
 	std::string convert(std::string_view _string) const override;
-
-	bool canBeConverted(std::string_view _string) const override;
 };
 
 class RunLengthEncodingConversion : public ConversionRule
 {
 public:
 	RunLengthEncodingConversion::RunLengthEncodingConversion();
+
+	std::string convert(std::string_view _string) const override;
+};
+
+class NumberToEnglishConversion : public ConversionRule
+{
+public:
+	NumberToEnglishConversion::NumberToEnglishConversion();
+
+	std::string convert(std::string_view _string) const override;
+};
+
+class AsRunLengthEncodingConversion : public ConversionRule
+{
+public:
+	AsRunLengthEncodingConversion::AsRunLengthEncodingConversion();
+
+	std::string convert(std::string_view _string) const override;
+};
+
+class LookAndSay : public ConversionRule
+{
+public:
+	LookAndSay::LookAndSay();
 
 	std::string convert(std::string_view _string) const override;
 };
