bc5709700e66dca086aa5a1799d53a0cb8ae8653
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-10-09 10:46:45 +0200

    simple poc permutations

--- src/one2IIImain.cpp
+++ src/one2IIImain.cpp
@@ -10,9 +10,22 @@
 #include "converter.hpp"
 #include "conversionrules.hpp"
 
+std::ostream& operator<<(std::ostream& _os, const std::vector<uint8_t>& _vec)
+{
+	_os << "[ ";
+	std::string sep = "";
+	for(auto& v : _vec)
+	{
+		_os << sep << static_cast<short>(v);
+		sep = ", ";
+	}
+	_os << " ]";
+	return _os;
+}
+
 int main()
 {
-	std::string numberSequence;
+	std::string numberSequence = "123456";//;//
 	while(isValidNumberSequence(numberSequence) == false)
 	{
 		std::cout << "enter a number sequence: ";
@@ -26,30 +39,34 @@ int main()
 	uint8_t digits = static_cast<uint8_t>(numberSequence.size());
 	for(uint8_t parts = digits; parts > 0; --parts)
 	{
-		const auto partitions = integerPartitions(digits, parts);
+		auto partitions = integerPartitions(digits, parts);
 		for(auto& partition : partitions)
 		{
-			const auto views = partitionString(numberSequence, partition);
-			if(views.size() == 0) continue;
-			std::vector<std::vector<std::string>> allConversions;
-			for(auto& view : views)
+			do
 			{
-				std::vector<std::string> viewConversions;
-				for(auto& c : converters)
+				std::cout << partition << std::endl;
+				const auto views = partitionString(numberSequence, partition);
+				if(views.size() == 0) continue;
+				std::vector<std::vector<std::string>> allConversions;
+				for(auto& view : views)
 				{
-					viewConversions.push_back(std::move(c->convert(view)));
+					std::vector<std::string> viewConversions;
+					for(auto& c : converters)
+					{
+						viewConversions.push_back(std::move(c->convert(view)));
+					}
+					allConversions.push_back(std::move(viewConversions));
 				}
-				allConversions.push_back(std::move(viewConversions));
-			}
-			for(size_t x = allConversions.front().size(); x > 0; --x)
-			{
-				std::cout << " | ";
-				for(size_t y = 0; y < allConversions.size(); ++y)
+				for(size_t x = allConversions.front().size(); x > 0; --x)
 				{
-					std::cout << allConversions[y][x-1] << " | ";
+					std::cout << " | ";
+					for(size_t y = 0; y < allConversions.size(); ++y)
+					{
+						std::cout << allConversions[y][x-1] << " | ";
+					}
+					std::cout << std::endl;
 				}
-				std::cout << std::endl;
-			}
+			} while(std::next_permutation(rbegin(partition), rend(partition)));
 			std::cout << std::endl;
 		}
 	}
