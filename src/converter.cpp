e466c1714b5870644c1841380ba88775726444fd
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-10-16 19:05:41 +0200

    estimatePosiibilities start

--- src/converter.cpp
+++ src/converter.cpp
@@ -4,11 +4,43 @@
  * 
  */
 
+#include <algorithm>
+
 #include "converter.hpp"
+#include "conversionrule.hpp"
+#include "conversionfunctions.hpp"
+#include "helperfunctions.hpp"
 
 size_t Converter::estimatePossibilities(const RuleBook& _rules, std::string_view _string)
 {
-	return 0;
+	size_t totalPossibilities = 0;
+	uint8_t stringLength = static_cast<uint8_t>(_string.size()); //TODO do something with strings that are to long somewhere
+	uint8_t minPartSize = static_cast<uint8_t>(std::clamp(_rules.getMinInputSize(), 1ULL, 255ULL));
+	uint8_t maxPartSize = static_cast<uint8_t>(std::clamp(_rules.getMaxInputSize(), 1ULL, 255ULL));//TODO make these prettier
+	for(uint8_t partCount = stringLength; partCount > 0; --partCount)
+	{
+		auto possiblePartitions = integerPartitions(stringLength, partCount, minPartSize, maxPartSize);
+		for(auto& partition : possiblePartitions)
+		{
+			//TODO add permutations to the mix
+			size_t partitionPossibilities = 1;
+			for(uint8_t partSize : partition)
+			{
+				size_t partSizePossibilities = 0;
+				for(auto& rule : _rules)
+				{
+					if( partSize >= ConversionRule::minInputSize(rule) &&
+						partSize <= ConversionRule::maxInputSize(rule))
+					{
+						++partSizePossibilities;
+					}
+				}
+				partitionPossibilities *= partSizePossibilities;
+			}
+			totalPossibilities += partitionPossibilities;
+		}
+	}
+	return totalPossibilities;
 }
 
 size_t Converter::calculatePossibilities(const RuleBook& _rules, std::string_view _string)
