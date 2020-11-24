bec503ddc4bea6ddb9bc834c6ecc998614d10f79
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-11-24 14:36:54 +0100

    simplemenu still missing exit and call to outside

--- inc/simplemenu.hpp
+++ inc/simplemenu.hpp
@@ -1,51 +1,42 @@
+/**
+ * @file simplemenu.hpp
+ * @brief include file for menu and state
+ * 
+ */
 
-/*
-
-infoscreen
-enter number sequence
-choose conversion rules
-choose output style
-display output
-
-simplemenu
-	view
-	callback*
-
-conversionmenustatemachine
-	callback(view)
-	states[] {{view, {view, view}}}
-			   text	  input text
-
-*/
+#pragma once
 
 #include <string>
 #include <vector>
 
-struct SimpleMenu
+class SimpleMenu
 {
+public:
+	SimpleMenu(std::string_view _text, std::string_view(*_callback)(std::string_view)) :
+		text{_text}, callback{_callback} {}
+	void show();
+private:
 	std::string_view text;
 	std::string_view(*callback)(std::string_view);
-	void show();
 };
 
-struct MenuState
+class MenuState
 {
-	std::string_view parseInput(std::string_view);
+public:
 	struct State
 	{
 		std::string_view text;
-		struct InputMap
+		struct InputAndState
 		{
 			std::string_view input;
 			std::string_view state;
 		};
-		std::vector<InputMap> inputmap;
+		std::vector<InputAndState> inputmap;
 	};
+public:
+	SimpleMenu makeMenu();
+	std::string_view parseInput(std::string_view);
+	void addState(State&& _state);
+private:
 	std::vector<State> states;
 };
-
-
-void testfunc()
-{
-	
-}
