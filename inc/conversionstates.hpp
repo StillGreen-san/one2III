c50e3037bcb35dfc99d81685fbf492198eebba55
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2020-11-26 17:56:59 +0100

    sample menu layout

--- inc/conversionstates.hpp
+++ inc/conversionstates.hpp
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#include <string>
+#include "simplemenu.hpp"
 
 //  struct SimpleState
 //  {
@@ -20,15 +20,88 @@
 //  	std::vector<Action> inputmap;
 //  };
 
-namespace Info
+//TODO support for dynamic text
+//TODO check if string can be char*
+
+namespace ConversionStates
 {
-	static const std::string text =
-		"\none2III\n"
-			"\tgiven a string of digits and a list of conversion rules\n"
-			"\tsplit the string into all possible substrings\n"
-			"\tconvert every substring using every possible conversion rule\n"
-		"\nenter anything to continue";
-	
-} // namespace Info
 
+static const std::string Info_text =
+	"\none2III\n"
+		"\tgiven a string of digits and a list of conversion rules\n"
+		"\tsplit the string into all possible substrings\n"
+		"\tconvert every substring using every possible conversion rule\n"
+	"\nenter anything to continue";
 
+static const std::string Test_text =
+	"\ntest menu\n"
+		"\ti: Info\n"
+		"\tt: This\n"
+		"\tc: Callback\n"
+		"\te: Exit\n"
+	"\nchoose one option";
+static const std::string Test_action_i = "i";
+static const std::string Test_action_t = "t";
+static const std::string Test_action_c = "c";
+static const std::string Test_action_e = "e";
+
+static const std::string Exit_text =
+	"\none2III\n"
+	"\nenter anything to exit";
+
+static const SimpleState Info
+{
+	Info_text,
+	{
+		{
+			nullptr,
+			nullptr,
+			Test_text
+		}
+	}
+};
+
+static size_t testCounter = 0;
+static void testCallback(std::string_view _string){
+	++testCounter;
+}
+static const SimpleState Test
+{
+	Test_text,
+	{
+		{
+			Test_action_t,
+			nullptr,
+			Test_text
+		},
+		{
+			Test_action_i,
+			nullptr,
+			Info_text
+		},
+		{
+			Test_action_c,
+			testCallback,
+			Info_text
+		},
+		{
+			Test_action_e,
+			nullptr,
+			Exit_text
+		}
+	}
+};
+
+static const SimpleState Exit
+{
+	Exit_text,
+	{
+		{
+			nullptr,
+			nullptr,
+			nullptr
+		}
+	}
+};
+
+} // namespace ConversionStates
