11f7e4f525f26ff0df81a88b17392afa3fd85e69
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2021-04-23 16:29:39 +0200

    NewSimpleMenu draft 0

--- inc/simplemenu.hpp
+++ inc/simplemenu.hpp
@@ -1,7 +1,7 @@
 /**
  * @file simplemenu.hpp
  * @brief include file for menu and state
- * 
+ *
  */
 
 #pragma once
@@ -18,14 +18,47 @@ public:
 		struct Action
 		{
 			std::string_view input;
-			bool(*external)(std::string_view);
+			bool (*external)(std::string_view);
 			std::string_view state;
 		};
 		std::vector<Action> inputmap;
 	};
 	void show(size_t _startingState = 0);
 	void addState(SimpleMenu::State&& _state);
+
 private:
 	std::vector<State> states;
 	size_t activeState = 0;
 };
+
+#include <functional>
+
+struct NewSimpleScreen
+{
+	NewSimpleScreen& description(std::string desc);
+	NewSimpleScreen& addOption(char key, std::string desc, int screen);
+	NewSimpleScreen& addOption(char key, std::string desc, std::function<void()> cllbck);
+	NewSimpleScreen& addOption(char key, std::string desc, void (NewSimpleMenu::*cllbck)());
+};
+
+struct NewSimpleMenu
+{
+	NewSimpleScreen& addScreen(int id);
+	void exit();
+	void restart();
+};
+
+void NSMtest()
+{
+	auto AAAAAA = NewSimpleMenu::exit;
+	NewSimpleMenu menu;
+	menu.addScreen(69).description("valuable text").addOption('k', "som optn", 42);
+	menu.addScreen(42)
+	    .description("valuable text")
+	    .addOption(
+	        'k', "som optn",
+	        []()
+	        {
+	        });
+	menu.addScreen(-1).description("valuable text").addOption('e', "baiii", NewSimpleMenu::exit);
+}
