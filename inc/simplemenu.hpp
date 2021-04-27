117ee95468aac48bfd78f7b42a1a54139e570b0d
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2021-04-27 12:32:10 +0200

    SimpleMenu replacement start

--- inc/simplemenu.hpp
+++ inc/simplemenu.hpp
@@ -1,32 +1,110 @@
 /**
  * @file simplemenu.hpp
- * @brief include file for menu and state
+ * @brief include file for menu
  *
  */
 
 #pragma once
 
+#include <functional>
 #include <string>
 #include <vector>
 
-class SimpleMenu
+/**
+ * @brief a simple console menu system
+ *
+ */
+struct SimpleMenu
 {
 public:
-	struct State
+	enum : int
 	{
-		std::string_view text;
-		struct Action
-		{
-			std::string_view input;
-			bool (*external)(std::string_view);
-			std::string_view state;
-		};
-		std::vector<Action> inputmap;
+		/**
+		 * @brief screen id to signal the menu should exit when the option is choosen
+		 *
+		 */
+		Exit = std::numeric_limits<int>::min(),
+
+		/**
+		 * @brief screen id to signal the menu should go to the screen specified with show() when the option is choosen
+		 *
+		 */
+		Restart,
+
+		/**
+		 * @brief screen id to signal the menu should go to this screen when the option is choosen
+		 *
+		 */
+		This,
+
+		/**
+		 * @brief key code to signal the menu should accept any input for this option
+		 *
+		 */
+		AnyKey
 	};
-	void show(size_t _startingState = 0);
-	void addState(SimpleMenu::State&& _state);
+
+	/**
+	 * @brief screen obj used with SimpleMenu
+	 *
+	 */
+	struct SimpleScreen
+	{
+	public:
+		/**
+		 * @brief adds the description displayed above the options
+		 *
+		 * @param desc the text to display
+		 * @return SimpleScreen&
+		 */
+		SimpleScreen& setDescription(std::string desc);
+
+		/**
+		 * @brief add an option
+		 *
+		 * @param key the key to press to select this option,
+		 * use SimpleMenu::AnyKey to allow any
+		 * @param desc the description for this option
+		 * @param screen the screen to transition to after this option was selected,
+		 * use SimpleMenu::Exit/Restart to exit/restart the menu
+		 * @param cllbck a function to call when this option was selected
+		 * @return SimpleScreen& this screen
+		 */
+		SimpleScreen& addOption(char key, std::string desc, int screen, std::function<void()> cllbck = nullptr);
+
+	private:
+		friend SimpleMenu;
+		explicit SimpleScreen(int id) : id{id} {};
+		struct Option
+		{
+			char key;
+			std::string description;
+			int nextScreen;
+			std::function<void()> callback;
+		};
+		int id;
+		std::string description;
+		std::vector<Option> options;
+	};
+
+	/**
+	 * @brief adds a screen to the menu,
+	 * use method chaining to set options for the screen,
+	 * if no options are added this screen will exit the menu
+	 *
+	 * @param id identifier for the screen
+	 * @return SimpleScreen& the new screen
+	 */
+	SimpleScreen& addScreen(int id);
+
+	/**
+	 * @brief enters the menu loop with the screen specified by id,
+	 * will exit imediatly if id was not found
+	 *
+	 * @param id the screen id to show
+	 */
+	void show(int id);
 
 private:
-	std::vector<State> states;
-	size_t activeState = 0;
+	std::vector<SimpleScreen> screens;
 };
--- src/simplemenu.cpp
+++ src/simplemenu.cpp
@@ -1,6 +1,6 @@
 /**
- * @file simplemenu.hpp
- * @brief include file for menu and state
+ * @file simplemenu.cpp
+ * @brief implementation file for menu
  *
  */
 
@@ -8,165 +8,52 @@
 
 #include <iostream>
 
-void SimpleMenu::show(size_t _startingState)
+SimpleMenu::SimpleScreen& SimpleMenu::SimpleScreen::setDescription(std::string desc)
 {
-	if(states.size() == 0)
-	{
-		return;
-	}
-	activeState = _startingState;
-
-	while(true)
-	{
-		std::cout << states[activeState].text << std::endl;
-
-		std::string input;
-		std::cin >> input;
-
-		bool handled = false;
-		for(auto& action : states[activeState].inputmap)
-		{
-			if(action.input.data() == nullptr || action.input == input)
-			{
-				if(action.external)
-				{
-					if(!action.external(input))
-					{
-						handled = true;
-						break;
-					}
-				}
-				size_t size = states.size();
-				for(activeState = 0; activeState < size; ++activeState)
-				{
-					if(states[activeState].text == action.state)
-					{
-						handled = true;
-						break;
-					}
-				}
-				break;
-			}
-		}
-		if(handled == false)
-		{
-			return;
-		}
-	}
-	return;
+	return *this;
 }
 
-void SimpleMenu::addState(SimpleMenu::State&& _state)
+SimpleMenu::SimpleScreen& SimpleMenu::SimpleScreen::addOption(
+    char key, std::string desc, int screen, std::function<void()> cllbck)
 {
-	states.emplace_back(_state);
+	return *this;
 }
 
-#include <functional>
-
-struct NewSimpleMenu
+SimpleMenu::SimpleScreen& SimpleMenu::addScreen(int id)
 {
-	enum : int
+	auto it = std::find_if(
+	    std::begin(screens), std::end(screens),
+	    [id](const SimpleScreen& screen)
+	    {
+		    return screen.id == id;
+	    });
+	if(it != std::cend(screens))
 	{
-		/**
-		 * @brief screen id to signal the menu should exit when the option is choosen
-		 *
-		 */
-		Exit = std::numeric_limits<int>::min(),
-
-		/**
-		 * @brief screen id to signal the menu should go to the screen specified with show() when the option is choosen
-		 *
-		 */
-		Restart
-	};
-	enum : char
-	{
-		/**
-		 * @brief key code to signal the menu should accept any input for this option
-		 *
-		 */
-		AnyKey = std::numeric_limits<char>::min()
-	};
-	struct NewSimpleScreen
-	{
-		/**
-		 * @brief adds the description displayed above the options
-		 *
-		 * @param desc the text to display
-		 * @return NewSimpleScreen&
-		 */
-		NewSimpleScreen& description(std::string desc)
-		{
-			return *this;
-		}
-
-		/**
-		 * @brief add an option
-		 *
-		 * @param key the key to press to select this option,
-		 * use SimpleMenu::AnyKey to allow any
-		 * @param desc the description for this option
-		 * @param screen the screen to transition to after this option was selected,
-		 * use SimpleMenu::Exit/Restart to exit/restart the menu
-		 * @return NewSimpleScreen& this screen
-		 */
-		NewSimpleScreen& addOption(char key, std::string desc, int screen)
-		{
-			return *this;
-		}
-
-		/**
-		 * @brief add an option
-		 *
-		 * @param key the key to press to select this option,
-		 * use SimpleMenu::AnyKey to allow any
-		 * @param desc the description for this option
-		 * @param cllbck a function to call when this option was selected
-		 * @return NewSimpleScreen& this screen
-		 */
-		NewSimpleScreen& addOption(char key, std::string desc, std::function<void()> cllbck)
-		{
-			return *this;
-		}
-	} TMP_REMOVE_ME;
-
-	/**
-	 * @brief adds a screen to the menu,
-	 * use method chaining to set options for the screen,
-	 * if no options are added this screen will exit the menu
-	 *
-	 * @param id identifier for the screen
-	 * @return NewSimpleScreen& the new screen
-	 */
-	NewSimpleScreen& addScreen(int id)
-	{
-		return TMP_REMOVE_ME;
+		return *it;
 	}
+	return screens.emplace_back(id);
+}
 
-	/**
-	 * @brief enters the menu loop with the screen specified by id,
-	 * will exit imediatly if id was not found
-	 *
-	 * @param id the screen id to show
-	 */
-	void show(int id)
-	{
-	}
-};
+void SimpleMenu::show(int id)
+{
+	//
+}
+
+// ##########################################
 
 void NSMtest()
 {
-	NewSimpleMenu menu;
-	menu.addScreen(69).description("valuable text").addOption('k', "som optn", 42);
+	SimpleMenu menu;
+	menu.addScreen(69).setDescription("valuable text").addOption('k', "som optn", 42);
 	menu.addScreen(42)
-	    .description("valuable text")
+	    .setDescription("valuable text")
 	    .addOption(
-	        'k', "som optn",
+	        'k', "som optn", SimpleMenu::This,
 	        []()
 	        {
 	        });
-	menu.addScreen(-1).description("valuable text").addOption('e', "baiii", NewSimpleMenu::Exit);
-	menu.addScreen(-2).description("valuable text").addOption(NewSimpleMenu::AnyKey, "baiii", NewSimpleMenu::Exit);
+	menu.addScreen(-1).setDescription("valuable text").addOption('e', "baiii", SimpleMenu::Exit);
+	menu.addScreen(-2).setDescription("valuable text").addOption(SimpleMenu::AnyKey, "baiii", SimpleMenu::Exit);
 
 	menu.show(-1);
 }
