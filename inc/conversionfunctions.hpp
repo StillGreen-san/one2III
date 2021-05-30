1491e00debe3f04745051d863fca64d2df5a080a
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2021-05-30 11:46:12 +0200

    add conversion example in description

--- inc/conversionfunctions.hpp
+++ inc/conversionfunctions.hpp
@@ -3,10 +3,10 @@
 #include <string>
 
 // TODO increase supported number sizes/ranges
-// TODO add example in descriptions?
 
 /**
- * @brief returns a number sequence encoded with RLE, space separated
+ * @brief returns a number sequence encoded with RLE, space separated.
+ * e.g. from '335' to '2:2 1:5'
  *
  * @param number 1<=len=>inf
  * @return std::string the converted string
@@ -14,7 +14,8 @@
 std::string runLengthEncode(std::string_view number);
 
 /**
- * @brief returns a number sequence decoded with RLE
+ * @brief returns a number sequence decoded with RLE.
+ * e.g. from '3211' to '2221'
  *
  * @param number 2<=len=>inf
  * @return std::string the converted string
@@ -22,7 +23,8 @@ std::string runLengthEncode(std::string_view number);
 std::string runLengthDecode(std::string_view number);
 
 /**
- * @brief returns a number sequence as roman numerals
+ * @brief returns a number sequence as roman numerals.
+ * e.g. from '123' to 'CXXIII'
  *
  * @param number "0" <=> "3999"
  * @return std::string the converted string
@@ -30,7 +32,8 @@ std::string runLengthDecode(std::string_view number);
 std::string romanNumerals(std::string_view number);
 
 /**
- * @brief returns a number in english words
+ * @brief returns a number in english words.
+ * e.g. from '321' to 'three hundred twenty one'
  *
  * @param number "0" <=> "999"
  * @return std::string the converted string
@@ -38,7 +41,8 @@ std::string romanNumerals(std::string_view number);
 std::string numberToEnglish(std::string_view number);
 
 /**
- * @brief returns a number sequence converted with run length encoding then to english words
+ * @brief returns a number sequence converted with run length encoding then to english words.
+ * e.g. from '770' to 'two sevens one zero'
  *
  * @param number 1<=len=>inf
  * @return std::string the converted string
--- inc/ruletype.hpp
+++ inc/ruletype.hpp
@@ -9,37 +9,43 @@
 enum class RuleType : uint8_t
 {
 	/**
-	 * @brief converts a number sequence to roman numerals
+	 * @brief converts a number sequence to roman numerals.
+	 * e.g. from '123' to 'CXXIII'
 	 *
 	 */
 	RomanNumeralConversion,
 
 	/**
-	 * @brief returns a number sequence as is
+	 * @brief returns a number sequence as is.
+	 * e.g. from '123' to '123'
 	 *
 	 */
 	AsNumberConversion,
 
 	/**
-	 * @brief converts a number sequence to english words
+	 * @brief converts a number sequence to english words.
+	 * e.g. from '321' to 'three hundred twenty one'
 	 *
 	 */
 	NumberToEnglishConversion,
 
 	/**
-	 * @brief converts a number sequence as run length encoding
+	 * @brief converts a number sequence as run length encoding.
+	 * e.g. from '335' to '2:2 1:5'
 	 *
 	 */
 	RunLengthEncodingConversion,
 
 	/**
-	 * @brief converts a number sequence as run length encoding
+	 * @brief converts a number sequence as run length encoding.
+	 * e.g. from '3211' to '2221'
 	 *
 	 */
 	RunLengthDecodingConversion,
 
 	/**
-	 * @brief converts a number sequence with run length encoding then to english words
+	 * @brief converts a number sequence with run length encoding then to english words.
+	 * e.g. from '770' to 'two sevens one zero'
 	 *
 	 */
 	LookAndSayConversion,
